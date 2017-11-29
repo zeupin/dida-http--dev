2cbc40dab02490ee1829f39ca1fe25fe3c46776b
Macc Liu <maccliu@qq.com>
2017-11-29 14:56:51 +0800

    redirect()支持刷新时间

--- src/Dida/Http/Response.php
+++ src/Dida/Http/Response.php
@@ -51,11 +51,19 @@ class Response
     /**
      * 重定向。
      *
-     * @param string $url
+     * @param string $url  重定向的网址，要包含http/https协议头。
+     * @param int|null $refresh  需要等待的时间。
+     *     如果为null，表示立即跳转，且不执行后续的php代码。
+     *     如果为int，表示等待$refresh时间后再跳转，此时会继续执行后续PHP代码。
      */
-    public static function redirect($url)
+    public static function redirect($url, $refresh = null)
     {
-        header("Location: $url");
+        if (is_numeric($refresh)) {
+            header("Refresh: $refresh; url=$url");
+        } else {
+            header("Location: $url", true, 301);
+            exit();
+        }
     }
 
 
