c0d0ae186e057328f36aeae0f9a9ee4a823816a7
Macc Liu <maccliu@qq.com>
2017-11-30 12:21:07 +0800

    新增获取客户端ip的方法getClientIP

--- src/Dida/Http/Request.php
+++ src/Dida/Http/Request.php
@@ -17,7 +17,7 @@ class Request
     /**
      * Version
      */
-    const VERSION = '20171129';
+    const VERSION = '20171130';
 
 
     /**
@@ -29,5 +29,27 @@ class Request
     {
         return (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) &&
             (strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest');
+    }
+
+
+    /**
+     * 获取客户端IP
+     *
+     * @return string|bool 正常返回读取的ip，异常返回false
+     */
+    public static function getClientIP()
+    {
+        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+            $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        } elseif (isset($_SERVER["HTTP_CLIENT_IP"])) {
+            $ip = $_SERVER["HTTP_CLIENT_IP"];
+        } elseif (isset($_SERVER["REMOTE_ADDR"])) {
+            $ip = $_SERVER["REMOTE_ADDR"];
+        } else {
+            $ip = false; // ip未定义
+        }
+
+        // 返回结果
+        return $ip;
     }
 }
