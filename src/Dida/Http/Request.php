01bc16c39cc675c24ae9a6c24f4029d8dabb59a2
Macc Liu <maccliu@qq.com>
2017-11-30 15:34:05 +0800

    新增若干方法

--- src/Dida/Http/Request.php
+++ src/Dida/Http/Request.php
@@ -19,6 +19,121 @@ class Request
      */
     const VERSION = '20171130';
 
+    /**
+     * @var array
+     * [
+     *    'path'     =>
+     *    'query'    =>
+     *    'fragment' =>
+     * ]
+     */
+    protected static $urlinfo = null;
+
+
+    /**
+     * 解析url
+     */
+    protected static function parseUrl()
+    {
+        // 不重复初始化
+        if (self::$urlinfo) {
+            return;
+        }
+
+        // path，query，fragment
+        self::$urlinfo = parse_url($_SERVER["REQUEST_URI"]);
+    }
+
+
+    /**
+     * Request的路径。
+     *
+     * @return string|null|false   正常返回path，没有path则返回null，出错返回false
+     */
+    public static function path()
+    {
+        if (self::$urlinfo === false) {
+            return false;
+        }
+
+        if (self::$urlinfo === null) {
+            self::parseUrl();
+        }
+
+        return isset(self::$urlinfo['path']) ? self::$urlinfo['path'] : null;
+    }
+
+
+    /**
+     * Request的查询串。
+     *
+     * @return string|null|false   正常返回查询串，没有则返回null，出错返回false
+     */
+    public static function queryString()
+    {
+        if (self::$urlinfo === false) {
+            return false;
+        }
+
+        if (self::$urlinfo === null) {
+            self::parseUrl();
+        }
+
+        return isset(self::$urlinfo['query']) ? self::$urlinfo['query'] : null;
+    }
+
+
+    /**
+     * Request的页面书签。
+     *
+     * @return string|null|false   正常返回fragment，没有则返回null，出错返回false
+     */
+    public static function fragment()
+    {
+        if (self::$urlinfo === false) {
+            return false;
+        }
+
+        if (self::$urlinfo === null) {
+            self::parseUrl();
+        }
+
+        return isset(self::$urlinfo['fragment']) ? self::$urlinfo['fragment'] : null;
+    }
+
+
+    /**
+     * 获取Request的method。
+     *
+     * 如果有POST的DIDA_REQUEST_METHOD字段，则以此字段为准。
+     * 没有这个字段，则看是普通的get还是post。
+     * 正常返回get，post，put，patch，delete，head，options之一；如果非法，返回false。
+     *
+     * @return string|false
+     */
+    public static function method()
+    {
+        if (isset($_POST['DIDA_REQUEST_METHOD'])) {
+            $method = strtolower($_POST['DIDA_REQUEST_METHOD']);
+        } elseif (isset($_SERVER['REQUEST_METHOD'])) {
+            $method = strtolower($_SERVER['REQUEST_METHOD']);
+        }
+
+        // 只能为：get，post，put，patch，delete，head，options之一
+        switch ($method) {
+            case 'get':      // 获取资源
+            case 'post':     // 新建资源
+            case 'put':      // 更新整个资源
+            case 'patch':    // 更新资源的个别字段
+            case 'delete':   // 删除资源
+            case 'head':     // 查询资源头
+            case 'options':  // 查询可选操作
+                return $method;
+            default:
+                return false;
+        }
+    }
+
 
     /**
      * 是否是Ajax请求
@@ -35,9 +150,9 @@ class Request
     /**
      * 获取客户端IP
      *
-     * @return string|bool 正常返回读取的ip，异常返回false
+     * @return string|false 正常返回读取到的ip，无法获取时，返回false
      */
-    public static function getClientIP()
+    public static function clientIP()
     {
         if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
             $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
@@ -51,5 +166,20 @@ class Request
 
         // 返回结果
         return $ip;
+    }
+
+
+    /**
+     * 获取Request的协议名(http/https)
+     *
+     * @return string|false 正常返回读取到的schema，无法获取时，返回false
+     */
+    public static function schema()
+    {
+        if (isset($_SERVER['REQUEST_SCHEME'])) {
+            return $_SERVER['REQUEST_SCHEME'];
+        } else {
+            return false;
+        }
     }
 }
