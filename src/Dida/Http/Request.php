c42409aa1762dd474421a95939984efb721ab4e4
Macc Liu <maccliu@qq.com>
2017-12-06 21:14:16 +0800

    优化clientIP()

--- src/Dida/Http/Request.php
+++ src/Dida/Http/Request.php
@@ -186,6 +186,10 @@ class Request
             $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
         } elseif (isset($_SERVER["HTTP_CLIENT_IP"])) {
             $ip = $_SERVER["HTTP_CLIENT_IP"];
+        } elseif (isset($_SERVER["HTTP_X_CLIENT_IP"])) {
+            $ip = $_SERVER["HTTP_X_CLIENT_IP"];
+        } elseif (isset($_SERVER["HTTP_X_CLUSTER_CLIENT_IP"])) {
+            $ip = $_SERVER["HTTP_X_CLUSTER_CLIENT_IP"];
         } elseif (isset($_SERVER["REMOTE_ADDR"])) {
             $ip = $_SERVER["REMOTE_ADDR"];
         } else {
